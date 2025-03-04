6cfd23de36ce4716367c42fbc5a049fafc7af219
bunnylin0 <69311935+bunnylin0@users.noreply.github.com>
2025-03-04 23:17:06 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -20,7 +20,7 @@ const CONFIG = {
   // 英雄区(首页顶部大卡)
   HEO_HERO_TITLE_1: '人生体验',
   HEO_HERO_TITLE_2: '与兴趣爱好',
-  HEO_HERO_TITLE_3: 'TANGLY1024.COM',
+  HEO_HERO_TITLE_3: 'BUNNYLIN.COM',
   HEO_HERO_TITLE_4: '新版上线',
   HEO_HERO_TITLE_5: 'NotionNext4.0 轻松定制主题',
   HEO_HERO_TITLE_LINK: 'https://tangly1024.com',
